517d5d92d46023e459c05e743992b0adf94ec706
Aristotelis-Stratis <stratis.dev@outlook.com>
2023-09-03 14:01:21 +0200

    game functions and restart button

--- script.js
+++ script.js
@@ -1,89 +1,177 @@
 let fields = [
     null,
-    'circle',
-    'circle',
     null,
     null,
     null,
-    'cross',
-    'cross',
+    null,
+    null,
+    null,
+    null,
     null,
 ];
 
-// Rufe die render-Funktion auf, um das Spielfeld zu initialisieren
+const WINNING_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+    [0, 4, 8], [2, 4, 6], // diagonal
+];
+
+let currentPlayer = 'circle';
+
 
 function init() {
     render();
 }
-
-// Funktion zum Rendern des Spielfelds
-function render() {
-    const contentDiv = document.getElementById('content');
-    const table = document.createElement('table');
-
-    for (let i = 0; i < 3; i++) {
-        const row = document.createElement('tr');
-        for (let j = 0; j < 3; j++) {
-            const cell = document.createElement('td');
-            const index = i * 3 + j;
-
-            // Wenn das Feld nicht null ist, füge das entsprechende Symbol hinzu
-            if (fields[index] === 'circle') {
-                const circleSVG = generateCircleSVG(); // Erzeuge das SVG-HTML als String
-                cell.innerHTML = circleSVG; // Füge das SVG-HTML zur Zelle hinzu
-                cell.classList.add('cell', 'circle');
-            } else if (fields[index] === 'cross') {
-                const crossSVG = generateCrossSVG(); // Erzeuge das SVG-HTML als String
-                cell.innerHTML = crossSVG;
-                cell.classList.add('cell', 'cross');
-            }
-
-            row.appendChild(cell);
-        }
-        table.appendChild(row);
-    }
-
-    // Füge die Tabelle dem Container hinzu
-    contentDiv.innerHTML = '';
-    contentDiv.appendChild(table);
+// Initialize the game
+function init() {
+    render();
 }
 
+// Render the game board
+function render() {
+    // Retrieve the content container
+    const contentDiv = document.getElementById('content');
 
+    // Generate the HTML table for the game board
+    let tableHtml = '<table>';
+    for (let i = 0; i < 3; i++) {
+        tableHtml += '<tr>';
+        for (let j = 0; j < 3; j++) {
+            const index = i * 3 + j;
+            let symbol = '';
+            if (fields[index] === 'circle') {
+                symbol = generateCircleSVG();
+            } else if (fields[index] === 'cross') {
+                symbol = generateCrossSVG();
+            }
+            // Create a cell with a clickable symbol container
+            tableHtml += `<td><div class="symbol-container" onclick="handleClick(this, ${index})">${symbol}</div></td>`;
+        }
+        tableHtml += '</tr>';
+    }
+    tableHtml += '</table>';
 
+    // Set the HTML for the content container
+    contentDiv.innerHTML = tableHtml;
+}
 
+// Handle a cell click
+function handleClick(cell, index) {
+    if (fields[index] === null && !isGameFinished()) {
+        fields[index] = currentPlayer;
+        cell.innerHTML = currentPlayer === 'circle' ? generateCircleSVG() : generateCrossSVG();
+        currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
+
+        if (isGameFinished()) {
+            const winCombination = getWinningCombination();
+            drawWinningLine(winCombination);
+            disableClicksOnBoard(); // Disable clicks on the game board
+        }
+    }
+}
+
+// Disable clicks on the game board
+function disableClicksOnBoard() {
+    const cells = document.querySelectorAll('td');
+    cells.forEach(cell => {
+        cell.onclick = null;
+    });
+}
+
+// Check if the game is finished
+function isGameFinished() {
+    return fields.every((field) => field !== null) || getWinningCombination() !== null;
+}
+
+// Get the winning combination
+function getWinningCombination() {
+    for (let i = 0; i < WINNING_COMBINATIONS.length; i++) {
+        const [a, b, c] = WINNING_COMBINATIONS[i];
+        if (fields[a] === fields[b] && fields[b] === fields[c] && fields[a] !== null) {
+            return WINNING_COMBINATIONS[i];
+        }
+    }
+    return null;
+}
+
+// Generate the SVG for the circle symbol
 function generateCircleSVG() {
-    const color = '#04ACE8';
+    const color = '#00B0EF';
     const width = 70;
     const height = 70;
 
-    return `<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="35" cy="35" r="30" stroke="${color}" stroke-width="5" fill="transparent">
-                <animateTransform
-                  attributeName="transform"
-                  type="rotate"
-                  from="0 35 35"
-                  to="360 35 35"
-                  dur="125ms"
-                  repeatCount="indefinite"
-                />
-                <animate attributeName="r" from="0" to="30" dur="250ms" fill="freeze" />
+    return `<svg width="${width}" height="${height}">
+              <circle cx="35" cy="35" r="30" stroke="${color}" stroke-width="5" fill="none">
+                <animate attributeName="stroke-dasharray" from="0 188.5" to="188.5 0" dur="0.2s" fill="freeze" />
               </circle>
             </svg>`;
 }
 
-
+// Generate the SVG for the cross symbol
 function generateCrossSVG() {
-    const color = '#FEC009';
+    const color = '#FFC000';
     const width = 70;
     const height = 70;
 
-    return `<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
-              <line x1="0" y1="0" x2="70" y2="70" stroke="${color}" stroke-width="5" stroke-dasharray="0 88.5">
-                <animate attributeName="stroke-dasharray" from="0 88.5" to="88.5 0" dur="250ms" fill="freeze" />
-              </line>
-              <line x1="70" y1="0" x2="0" y2="70" stroke="${color}" stroke-width="5" stroke-dasharray="0 88.5">
-                <animate attributeName="stroke-dasharray" from="0 88.5" to="88.5 0" dur="250ms" fill="freeze" />
-              </line>
-            </svg>`;
+    const svgHtml = `
+      <svg width="${width}" height="${height}">
+        <line x1="0" y1="0" x2="${width}" y2="${height}"
+          stroke="${color}" stroke-width="5">
+          <animate attributeName="x2" values="0; ${width}" dur="200ms" />
+          <animate attributeName="y2" values="0; ${height}" dur="200ms" />
+        </line>
+        <line x1="${width}" y1="0" x2="0" y2="${height}"
+          stroke="${color}" stroke-width="5">
+          <animate attributeName="x2" values="${width}; 0" dur="200ms" />
+          <animate attributeName="y2" values="0; ${height}" dur="200ms" />
+        </line>
+      </svg>
+    `;
+
+    return svgHtml;
 }
 
+// Draw the winning line
+function drawWinningLine(combination) {
+    const lineColor = '#ffffff';
+    const lineWidth = 5;
+
+    const startCell = document.querySelectorAll(`td`)[combination[0]];
+    const endCell = document.querySelectorAll(`td`)[combination[2]];
+    const startRect = startCell.getBoundingClientRect();
+    const endRect = endCell.getBoundingClientRect();
+
+    const contentRect = document.getElementById('content').getBoundingClientRect();
+
+    const lineLength = Math.sqrt(
+        Math.pow(endRect.left - startRect.left, 2) + Math.pow(endRect.top - startRect.top, 2)
+    );
+    const lineAngle = Math.atan2(endRect.top - startRect.top, endRect.left - startRect.left);
+
+    const line = document.createElement('div');
+    line.style.position = 'absolute';
+    line.style.width = `${lineLength}px`;
+    line.style.height = `${lineWidth}px`;
+    line.style.backgroundColor = lineColor;
+    line.style.top = `${startRect.top + startRect.height / 2 - lineWidth / 2 - contentRect.top}px`;
+    line.style.left = `${startRect.left + startRect.width / 2 - contentRect.left}px`;
+    line.style.transform = `rotate(${lineAngle}rad)`;
+    line.style.transformOrigin = `top left`;
+    document.getElementById('content').appendChild(line);
+}
+
+
+function restartGame() {
+    fields = [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+    ];
+    render();
+}
